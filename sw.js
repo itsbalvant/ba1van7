8241a15cdce9f80597160c8d6276c8dc0f4915a3
Balvant Chavda <109503483+itsbalvant@users.noreply.github.com>
2026-08-13 13:42:01 +0530

    Retitle Instagram philosophy essay

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Balvant Chavda's website
-const CACHE_NAME = 'balvant-site-cache-v4';
+const CACHE_NAME = 'balvant-site-cache-v5';
 const urlsToCache = [
   '/',
   '/index.html',
